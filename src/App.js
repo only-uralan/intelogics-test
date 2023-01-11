0756a241e10bdf3f7959892203fa2d747714ff0f
a.lidzhiev2 <arsik0990@gmail.com>
2023-01-11 17:39:26 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Table } from "antd";
 import { useSelector } from "react-redux";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import "./App.css";
 
 function App() {
@@ -37,25 +38,12 @@ function App() {
     <div className="App">
       <div className="table">
         <Table dataSource={apps} columns={columns} />
-        <div className="map">
-          <MapContainer
-            center={[59.84660399, 30.29496392]}
-            zoom={13}
-            scrollWheelZoom={false}
-          >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[51.505, -0.09]}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-          </MapContainer>
-        </div>
       </div>
-      <div className="map"></div>
+      <div className="leaflet-container">
+        <MapContainer center={position} zoom={13}>
+          <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+        </MapContainer>
+      </div>
     </div>
   );
 }
